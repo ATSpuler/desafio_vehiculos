106cddce88a61f66f305d5535f669add087aebfd
al <al@mail.com>
2024-08-06 13:37:19 -0400

    2. App: car_driven_admin, creada y linkeada, db configurada. #TODO: crear usuario y privilegios de la base de datos, migrar

---
+++ create_3_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Vehiculo_tb (
+  patente CHAR(6) PRIMARY KEY,
+  marca CHAR(20) NOT NULL,
+  modelo CHAR(20) NOT NULL,
+  year INT NOT NULL
+);
+
+CREATE TABLE Chofer_tb (
+  rut CHAR(9) PRIMARY KEY,
+  nombre CHAR(50) NOT NULL,
+  apellido CHAR(50) NOT NULL,
+  activo BOOLEAN DEFAULT FALSE,
+  creacion_registro DATE,
+  vehiculo_id CHAR(6) UNIQUE,
+  FOREIGN KEY (vehiculo_id) REFERENCES Vehiculo_tb(patente)
+);
+
+CREATE TABLE Reg_Cont_tb (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  fecha_compra DATE NOT NULL,
+  valor FLOAT NOT NULL,
+  vehiculo_id CHAR(6) NOT NULL UNIQUE,
+  FOREIGN KEY (vehiculo_id) REFERENCES Vehiculo_tb(patente)
+);
+
--- desafio_vehiculos/settings.py
+++ desafio_vehiculos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'car_driver_admin'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'desafio_vehiculos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desafio_2_db',
+        'USER': 'al',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
